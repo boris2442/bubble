fb4b318d2a693fbe8f9a6fa9d3e36fc4de146399
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-25 12:09:21 +0100

    text

---
+++ script.js
@@ -0,0 +1,14 @@
+//initialisation des variable && constantes
+
+const counterDisplay=document.querySelector("h3");
+
+let counter =0;
+const bubble=document.createElement("span");
+
+bubble.classList.add("bubble");
+
+document.body.appendChild(bubble);
+
+console.log(Math.random()*1000)
+
+ 
