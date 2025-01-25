57be7cbc75330f2d134789602d8624a3a32f005f
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-25 12:16:41 +0100

    suite codage

--- script.js
+++ script.js
@@ -10,5 +10,10 @@ bubble.classList.add("bubble");
 document.body.appendChild(bubble);
 
 console.log(Math.random()*1000)
+// let size= Math.random()*200 +100 + "px";
+let size=`${Math.random()*200 +100}px `
+console.log(size)
 
+bubble.style.width=size
+bubble.style.height=size
  
