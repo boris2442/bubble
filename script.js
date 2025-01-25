f8bbcc34dbc7a2fefbd7ddf0b8794eda70664da7
boris2442 <aubinborissimotsebo@gmail.com>
2025-01-25 13:25:51 +0100

    suite codage

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 //initialisation des variable && constantes
 
-const counterDisplay=document.querySelector("h3");
+const counterDisplay = document.querySelector("h3");
 
-let counter =0;
-const bubble=document.createElement("span");
+let counter = 0;
 
-bubble.classList.add("bubble");
+const bubleMaker = () => {
+    const bubble = document.createElement("span");
+  bubble.classList.add("bubble");
 
-document.body.appendChild(bubble);
+  document.body.appendChild(bubble);
 
-console.log(Math.random()*1000)
-// let size= Math.random()*200 +100 + "px";
-let size=`${Math.random()*200 +50}px `
-// console.log(size)
+//   console.log(Math.random() * 100);
+  // let size= Math.random()*200 +100 + "px";
+  let size = `${Math.random() * 200 + 100}px `;
+  // console.log(size)
 
-bubble.style.width=size;
-bubble.style.height=size;
-bubble.style.top=Math.random()*100 + "%";
-bubble.style.left=Math.random()*100 + "%";
- 
+  bubble.style.width = size;
+  bubble.style.height = size;
+  bubble.style.top = Math.random() * 100 +50+ "%";
+  bubble.style.left = Math.random() * 100 + "%";
 
-const plusMinus=Math.random()>0.5? 1:-1;
-bubble.style.setProperty('--left', Math.random()*100*plusMinus + "%")
+  const plusMinus = Math.random() > 0.5 ? 1 : -1;
+  bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+
+  bubble.addEventListener("click", () => {
+      counter++;
+      counterDisplay.textContent = counter;
+      bubble.remove()
+  });
+};
+
+
+setInterval(bubleMaker, 2000);
